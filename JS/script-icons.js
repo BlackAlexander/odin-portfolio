9a572da865644d713ce5065f1e6cd9be7cc7ff1f
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-07-21 14:44:05 +0300

    Big Improvements for Design and Responsiveness

--- JS/script-icons.js
+++ JS/script-icons.js
@@ -5,3 +5,25 @@ function changeClass(element) {
 function changeClassBack(element) {
     element.classList.remove("fa-bounce");
 }
+
+function adjustBrightness() {
+    console.log(window.innerWidth);
+    let brightnessValue;
+    if (window.innerWidth > 1000){
+        brightnessValue = 0;
+    } else if (window.innerWidth < 750) {
+        brightnessValue = 10;
+    } else {
+        brightnessValue = -(0.4)*(window.innerWidth)+400;
+        brightnessValue /= 10;
+        console.log(brightnessValue);
+    }
+    const socialIcons = document.querySelectorAll('.social-icons a');
+    socialIcons.forEach(icon => {
+        icon.style.filter = `brightness(${brightnessValue})`;
+    });
+}
+
+adjustBrightness();
+window.addEventListener('resize', adjustBrightness);
+
